60ef4170d15eef69194606cfc2a2479392547def
Serife Erguner <seriferabia@gmail.com>
2020-06-04 12:23:00 +0200

    A new method is added.

--- lab-info-system/lims-backend/src/main/java/com/company/limsbackend/persistence/repository/PersonRepository.java
+++ lab-info-system/lims-backend/src/main/java/com/company/limsbackend/persistence/repository/PersonRepository.java
@@ -3,5 +3,8 @@ package com.company.limsbackend.persistence.repository;
 import com.company.limsbackend.persistence.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<Person> findByEmail(String email);
 }
